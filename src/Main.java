144be4ff23ac04dbb049d0e8550c7dbda0a9044e
Tobikazi <117998999+Tobikazi@users.noreply.github.com>
2022-12-02 14:17:35 +0300

    Insert code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Queue<Person> queue = new LinkedList<>(generateClients());
+
+        while (!queue.isEmpty()) {
+            Person person = queue.poll();
+            System.out.printf("%s %s прокатился на аттракционе, ", person.getName(), person.getSureName());
+            int quantityTickets = person.getQuantityTickets();
+            if (person.getQuantityTickets() > 0){
+                --quantityTickets;
+                System.out.printf("%s %s идёт на аттракцион, у него осталось %d билет(а/ов)\n", person.getName(), person.getSureName(), quantityTickets);
+            person.setQuantityTickets(quantityTickets);
+            queue.add(person);
+                System.out.println();
+            }
+        }
+    }
+        private static List<Person> generateClients() {
+            return List.of(
+                    new Person("Иван", "Иванов", 4),
+                    new Person("Алексей", "Алексеев", 2),
+                    new Person("Игорь", "Угрев", 3),
+                    new Person("Уголь", "Угольков", 1)
+            );
     }
 }
