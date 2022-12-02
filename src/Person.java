3485fd6119e42e099d4c7b7b74a6d442840672bc
Tobikazi <117998999+Tobikazi@users.noreply.github.com>
2022-12-02 14:18:06 +0300

    insert code

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Person {
+import java.util.List;
+
+public class Person {
+
+    private String name;
+    private String sureName;
+
+
+
+    private Integer quantityTickets;
+
+    public Person(String name, String sureName, int quantityTickets) {
+        this.name = name;
+        this.sureName = sureName;
+        this.quantityTickets = quantityTickets;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSureName() {
+        return sureName;
+    }
+
+    public void setSureName(String sureName) {
+        this.sureName = sureName;
+    }
+
+    public Integer getQuantityTickets() {
+        return quantityTickets;
+    }
+
+    public void setQuantityTickets(Integer quantityTickets) {
+        this.quantityTickets = quantityTickets;
+    }
 }
